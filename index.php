5238cad701d1a728792d649c5e04900d7c3233ff
Giovanna <gigibernadelli001@gmail.com>
2023-05-05 15:02:23 -0300

    criação do Index

---
+++ index.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="bibliotecas/bootstrap-5.3.0/css/bootstrap.min.css" rel="stylesheet">
+  </head>
+  <body>
+    <h1>Hello, world!</h1>
+    <script src="bibliotecas/bootstrap-5.3.0/js/bootstrap.bundle.min.js"></script>
+  </body>
+</html>
