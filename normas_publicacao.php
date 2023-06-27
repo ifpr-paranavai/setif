2ba6424f8d07f3499557f56e1ba7f69a35b32046
a <a>
2023-06-27 07:54:55 -0300

    normas publicação

---
+++ normas_publicacao.php
@@ -0,0 +1,111 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+  <title>Normas de Publicação</title>
+  <?php include_once 'includes/metadados.php' ?>
+</head>
+
+<body>
+  <?php include_once 'includes/navbar.php' ?>
+  <?php include_once 'includes/rodape.php' ?>
+
+  </head>
+
+  <body>
+    <?php
+    include_once 'includes/navbar.php';
+    ?>
+    <style>
+      .custom-background {
+        background-color: #464BA4;
+      }
+
+      .custom-height {
+        height: 45%;
+      }
+
+      .custom-font {
+        font-family: 'Open Sans', sans-serif;
+      }
+    </style>
+    <div class="container px-0 m-0">
+      <div class="row">
+        <div class="col">
+          <div class="datas importantes custom-background h-max-content d-block d-inline-block d-flex p-10px text-white d-flex flex-column p-10">
+            <h1 class=" fs-4 text-white font-weight-bold mt-1 mx-auto"><img src="./imagens/calendario.png" class="mx-2" alt="">Datas Importantes</h1>
+            <br>
+            <h4 class="mx-auto custom-font">Data limite para a submissão de trabalhos:</h4>
+            <h4 class="mx-auto custom-font font-weight-light">10 de Outubro de 2023</h4>
+            <br>
+
+            <h4 class="mx-auto custom-font">Notificação aos autores:</h4>
+            <h4 class="mx-auto custom-font font-weight-light">24 de outubro de 2023</h4>
+            <br>
+
+            <h4 class="mx-auto custom-font">Submissão da versão final:</h4>
+            <h4 class="mx-auto custom-font font-weight-light">31 de outubro de 2023</h4>
+            <br>
+
+            <h4 class="mx-auto custom-font">Evento:</h4>
+            <h4 class="mx-auto custom-font font-weight-light">07 a 11 de novembro de 2023</h4>
+            <br>
+
+            <h4 class="mx-auto custom-font">Data prevista da mostra de trabalhos:</h4>
+            <h4 class="mx-auto custom-font font-weight-light">07 e 09 de dezembro de 2023</h4>
+            <br>
+          </div>
+        </div>
+
+        <div class="col">
+          <div class="submissao col w-60 h-max-content d-flex p-10px col-7 d-block d-flex flex-column">
+            <h1 class="text-dark text-weight-bold">Submissão</h1>
+            <h5 class="text-body-secondary text-weight-lighter">No sistema, cada submissão deve ser feita em duas etapas:
+              1.Clicar em Enviar Submissão para cadastrar o artigo e autores (lembrar de salvar o ID e a senha do artigo). <br>
+              2.Clicar em Enviar Arquivo para fazer upload do artigo (somente formato pdf). <br>
+              <br>
+              Sistema de sumissão: <a href="" class="rounded-pill btn btn-light btn-outline-success" role=" button">Clique aqui</a>
+            </h5>
+            <br>
+            <h1 class="text-dark text-weight-bold">Artigos completos:</h1>
+            <h5 class="text-body-secondary text-weight-lighter">No sistema, cada submissão deve ser feita em duas etapas:
+              Os artigos completos devem possuir no máximo 12 páginas. A formatação dos artigos deve seguir o modelo da SBC para artigos acadêmicos:
+              <h5 class="text-body-secondary text-weight-bold">Utilizar preferencialmente o LaTeX: Overleaf</h5>
+              <br>
+              Modelo SBC-Artigo: <a href="" class="rounded-pill btn btn-light btn-outline-success" role=" button">Clique aqui</a>
+            </h5>
+            <br>
+
+            <h1 class="text-dark text-weight-bold">Resumos:</h1>
+            <h5 class="text-body-secondary text-weight-lighter">No sistema, cada submissão deve ser feita em duas etapas:
+              Os artigos completos devem possuir no máximo 12 páginas. A formatação dos artigos deve seguir o modelo da SBC para artigos acadêmicos:
+              <h5 class="text-body-secondary text-weight-bold">Utilizar preferencialmente o LaTeX: Overleaf <br>
+                Não incluir abstract e o resumo no modelo latex</h5>
+              <br>
+              Modelo SBC-Resumo: <a href="" class="rounded-pill btn btn-light btn-outline-success" role=" button">Clique aqui</a>
+            </h5>
+            <br>
+
+          </div>
+
+        </div>
+
+      </div>
+
+    </div>
+
+    <div class="apresentacao" style="display: block;padding:10px">
+      <p>aa</p>
+      <p>aa</p>
+    </div>
+
+    <div class="topico interesse" style="background-color: #2EA75A; display:block; padding:10px">
+      <p>aa</p>
+      <p>aa</p>
+    </div>
+
+
+    <?php include_once 'includes/scripts.php' ?>
+  </body>
+
+</html>
