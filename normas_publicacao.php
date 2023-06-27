8d2f8c4abbdeecd74870c036f3f00fa5e3902ed4
a <a>
2023-06-27 08:29:33 -0300

    a

--- normas_publicacao.php
+++ normas_publicacao.php
@@ -28,6 +28,10 @@
       .custom-font {
         font-family: 'Open Sans', sans-serif;
       }
+
+      .custom-color {
+        color: #D9D9D9;
+      }
     </style>
     <div class="container px-0 m-0">
       <div class="row">
@@ -58,19 +62,18 @@
         </div>
 
         <div class="col">
-          <div class="submissao col w-60 h-max-content d-flex p-10px col-7 d-block d-flex flex-column">
+          <div class="submissao col w-60 h-max-content d-flex p-10px d-flex flex-column">
             <h1 class="text-dark text-weight-bold">Submissão</h1>
-            <h5 class="text-body-secondary text-weight-lighter">No sistema, cada submissão deve ser feita em duas etapas:
+            <h5 class="text-body-secondary text-weight-lighter">No sistema, cada submissão deve ser feita em duas etapas: <br>
               1.Clicar em Enviar Submissão para cadastrar o artigo e autores (lembrar de salvar o ID e a senha do artigo). <br>
               2.Clicar em Enviar Arquivo para fazer upload do artigo (somente formato pdf). <br>
-              <br>
               Sistema de sumissão: <a href="" class="rounded-pill btn btn-light btn-outline-success" role=" button">Clique aqui</a>
             </h5>
             <br>
             <h1 class="text-dark text-weight-bold">Artigos completos:</h1>
             <h5 class="text-body-secondary text-weight-lighter">No sistema, cada submissão deve ser feita em duas etapas:
               Os artigos completos devem possuir no máximo 12 páginas. A formatação dos artigos deve seguir o modelo da SBC para artigos acadêmicos:
-              <h5 class="text-body-secondary text-weight-bold">Utilizar preferencialmente o LaTeX: Overleaf</h5>
+              Utilizar preferencialmente o LaTeX: Overleaf
               <br>
               Modelo SBC-Artigo: <a href="" class="rounded-pill btn btn-light btn-outline-success" role=" button">Clique aqui</a>
             </h5>
@@ -79,8 +82,8 @@
             <h1 class="text-dark text-weight-bold">Resumos:</h1>
             <h5 class="text-body-secondary text-weight-lighter">No sistema, cada submissão deve ser feita em duas etapas:
               Os artigos completos devem possuir no máximo 12 páginas. A formatação dos artigos deve seguir o modelo da SBC para artigos acadêmicos:
-              <h5 class="text-body-secondary text-weight-bold">Utilizar preferencialmente o LaTeX: Overleaf <br>
-                Não incluir abstract e o resumo no modelo latex</h5>
+              Utilizar preferencialmente o LaTeX: Overleaf
+              Não incluir abstract e o resumo no modelo latex
               <br>
               Modelo SBC-Resumo: <a href="" class="rounded-pill btn btn-light btn-outline-success" role=" button">Clique aqui</a>
             </h5>
@@ -91,12 +94,10 @@
         </div>
 
       </div>
-
     </div>
 
-    <div class="apresentacao" style="display: block;padding:10px">
-      <p>aa</p>
-      <p>aa</p>
+    <div class="apresentacao display: block p-10 m-0">
+      <hr class="custom-color">
     </div>
 
     <div class="topico interesse" style="background-color: #2EA75A; display:block; padding:10px">
