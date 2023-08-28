f6940182934a826542de4ec30939235a007c18f1
Gigii55 <gigibernadelli001@gmail.com>
2023-08-28 17:02:47 -0300

    anais eventos

--- anaisEventos.php
+++ anaisEventos.php
@@ -23,22 +23,79 @@ include_once("./includes/navbar.php");
 
 <body>
     <div class="container px-auto py-5 mx-auto">
-        <a href="./anais/anais2022.php" class="fs-2 btn rounded-4 btn-primary mx-10 w-45 h-45 text-center py-3">Anais
+   
+
+        <a href="./anais/anais2022.php" class="mx-4 botao-estilo fs-2 btn rounded-4 btn-primary mx-10 w-45 h-45 text-center py-3 p-5">Anais
             2022</a>
-        <a href="./anais/anais2021.php" class="fs-2 btn rounded-4 btn-primary mx-10 w-45 h-45 text-center py-3">Anais
+        <a href="./anais/anais2021.php" class="mx-4 botao-estilo2 fs-2 btn rounded-4 btn-primary mx-10 w-45 h-45 text-center py-3 p-5">Anais
             2021</a>
-        <a href="./anais/anais2020.php" class="fs-2 btn rounded-4 btn-primary mx-10 w-45 h-45 text-center py-3">Anais
+        <a href="./anais/anais2020.php" class="mx-4 botao-estilo3 fs-2 btn rounded-4 btn-primary mx-10 w-45 h-45 text-center py-3 p-5">Anais
             2020</a>
-        <a href="./anais/anais2019.php" class="fs-2 btn rounded-4 btn-primary mx-10 w-45 h-45 text-center py-3">Anais
+        <a href="./anais/anais2019.php" class="mx-4 botao-estilo4 fs-2 btn rounded-4 btn-primary mx-10 w-45 h-45 text-center py-3 p-5">Anais
             2019</a>
         <br>
         <br>
-        <a href="./anais/anais2018.php" class="fs-2 btn rounded-4 btn-primary mx-10 w-45 h-45 text-center py-3">Anais
+        <a href="./anais/anais2018.php" class=" botao-estilo5 fs-2 btn rounded-4 btn-primary mx-10 w-45 h-45 text-center py-3 p-5">Anais
             2018</a>
-        <a href="./anais/anais2017.php" class="fs-2 btn rounded-4 btn-primary mx-10 w-45 h-45 text-center py-3">Anais
+        <a href="./anais/anais2017.php" class="mx-4 botao-estilo6 fs-2 btn rounded-4 btn-primary mx-10 w-45 h-45 text-center py-3 p-5">Anais
             2017</a>
-        <a href="./anais/anais2016.php" class="fs-2 btn rounded-4 btn-primary mx-10 w-45 h-45 text-center py-3">Anais
+        <a href="./anais/anais2016.php" class="mx-4 botao-estilo7 fs-2 btn rounded-4 btn-primary mx-10 w-45 h-45 text-center py-3 p-5">Anais
             2016</a>
+
+        <style>    
+
+        .botao-estilo{
+    background-color: #183276;
+    margin-left: 50px;
+}
+.botao-estilo:hover {
+    background-color: #A6B9EB;
+    
+}
+.botao-estilo2{
+    background-color: #223B7B;
+   
+}
+.botao-estilo2:hover {
+    background-color: #A6B9EB;
+}
+.botao-estilo3{
+    background-color: #354F92;
+ 
+}
+.botao-estilo3:hover {
+    background-color: #A6B9EB;
+}
+.botao-estilo4{
+    background-color: #435C9D;
+   
+}
+.botao-estilo4:hover {
+    background-color: #A6B9EB;
+}
+.botao-estilo5{
+    background-color: #6279B8;  
+    margin-left: 200px;
+}
+.botao-estilo5:hover {
+    background-color: #A6B9EB;
+}
+.botao-estilo6{
+    background-color: #748AC6;  
+    
+}
+.botao-estilo6:hover {
+    background-color: #A6B9EB;
+}
+.botao-estilo7{
+    background-color: #A6B9EB;  
+    
+}
+.botao-estilo7:hover {
+    background-color: #DAE0EF;
+}
+</style>
+
     </div>
 
 
@@ -52,3 +109,4 @@ include_once("./includes/navbar.php");
         integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
         crossorigin="anonymous"></script>
 </body>
+</html>
