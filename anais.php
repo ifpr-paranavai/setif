28c6cb6262686d1d52cb11bae64d7d4a419d3a48
Gigii55 <gigibernadelli001@gmail.com>
2023-09-15 16:53:37 -0300

    ajustes rodape

--- anais.php
+++ anais.php
@@ -4,7 +4,7 @@ require_once 'admin/includes/init.php';
 include_once LIB_CONTROLLER . DS . 'AnaisController.class.php';
 $controller = new AnaisController();
 $anos = $controller->getAnosDasEdicoesAnteriores();
-print_r($anos);
+
 
 ?>
 
@@ -21,26 +21,28 @@ print_r($anos);
     <?php
     include_once 'includes/navbar.php';
     ?>
-    <div class="container text-center mt-5 pt-5">
-        <div class="row align-items-center">
-            <?php
-            $quantidadeEdicoes = sizeof($anos);
-            $porcentagem = 1 / $quantidadeEdicoes;
-            $opacidade = $porcentagem;
-            foreach ($anos as $ano):
+   
+        <div class="container text-center mt-5 pt-5">
+            <div class="row align-items-center">
+                <?php
+                $quantidadeEdicoes = sizeof($anos);
+                $porcentagem = 1 / $quantidadeEdicoes;
+                $opacidade = $porcentagem;
+                foreach ($anos as $ano):
 
+                    ?>
+                    <div class="col-12 col-sm-6 col-md-3 py-2">
+                        <a href="anais.php/<?= $ano ?>" style="opacity:<?= $opacidade ?>"
+                            class="mx-4 fs-2 btn rounded-4 botao-anais mx-10 w-45 h-45 text-center py-3 p-5">Anais
+                            <?= $ano ?> </a>
+                    </div>
+                    <?php
+                    $opacidade += $porcentagem;
+                endforeach;
                 ?>
-            <div class="col-12 col-sm-6 col-md-3 py-2">
-                <a href="anais.php/<?= $ano ?>" style="opacity:<?= $opacidade ?>"
-                    class="mx-4 fs-2 btn rounded-4 botao-anais mx-10 w-45 h-45 text-center py-3 p-5">Anais
-                    <?= $ano ?> </a>
             </div>
-            <?php
-                $opacidade += $porcentagem;
-            endforeach;
-            ?>
         </div>
-    </div>
+   
     <?php include_once 'includes/rodape.php' ?>
     <?php include_once 'includes/scripts.php' ?>
 </body>
