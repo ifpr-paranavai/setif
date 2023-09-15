f228c1a0bb9b658362990e1caa2319e9126a2a5b
Marcelo F. Terenciani <marcelo.terenciani@gmail.com>
2023-09-15 16:48:13 -0300

    Rodapé parcial

--- includes/rodape.php
+++ includes/rodape.php
@@ -1,36 +1,32 @@
-<?php 
-    require_once 'admin/includes/init.php'; 
-    include_once LIB_CONTROLLER.DS.'IndexController.class.php';
+<?php
+require_once 'admin/includes/init.php';
+include_once LIB_CONTROLLER . DS . 'IndexController.class.php';
 
-    $controller = new IndexController();
-    $dados = $controller->getDadosPaginaInstitucional(2023);
+$controller = new IndexController();
+$dados = $controller->getDadosPaginaInstitucional(2023);
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
 
-<body>
-    <div class="bg-success p-2 text-white" style="height: auto; width: auto;">
-
-     <div class="d-flex flex-row mt-4">
-        <div class=""><i class="bi bi-person-fill d-block" style="font-size: 200%;"></i></div>
-        <div class="mx-2 fs-4 mb-2 my-2">Contatos</div>    
-     </div>
-
-     <div class="d-flex align-bottom flex-row">
-      <div><i class="bi bi-envelope-at-fill" style="font-size: 150%;"> </i> </div>
-      <div class="mx-2 my-2"><?= $dados->getEmail()?></div>
-      
-      <div><i class="bi bi-telephone-fill ms-2"  style="font-size: 150%;"></i> </div>
-      <div class="mx-2 my-2"><?= $dados->getTelefone()?></div>
+<footer class="footer py-3 bg-success text-white">
+    <div class="d-flex flex-row my-4 justify-content-center">
+        <i class="bi bi-person-fill d-block" style="font-size: 200%;">Contatos</i>
     </div>
+    <ul class="nav justify-content-center border-bottom pb-3 mb-3">
+        <li class="nav-item">
+            <a href="#" class="nav-link px-2 text-white">
+                <i class="bi bi-envelope-at-fill" style="font-size: 150%;"></i>
+                <?= $dados->getEmail() ?>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a href="#" class="nav-link px-2 text-white">
+                <i class="bi bi-telephone-fill ms-2" style="font-size: 150%;"></i>
+                <?= $dados->getTelefone() ?>
+            </a>
+        </li>
+    </ul>
 
-</body>
-
-</html>
+    <p class="text-center">© SETIF 2023 - Desenvolvido por Giovanna Bernadelli e Maria Eduarda F. Bruning
+    </p>
+</footer>
