91d58f73688768a18c5faab7e672fc06df459df8
Marcelo F. Terenciani <marcelo.terenciani@gmail.com>
2023-09-15 16:45:47 -0300

    Mudanças que não foram mudanças

--- anais.php
+++ anais.php
@@ -21,9 +21,8 @@ print_r($anos);
     <?php
     include_once 'includes/navbar.php';
     ?>
-
-    <div class="container mt-5 py-5 h-100">
-        <div calss="row align-items-center">
+    <div class="container text-center mt-5 pt-5">
+        <div class="row align-items-center">
             <?php
             $quantidadeEdicoes = sizeof($anos);
             $porcentagem = 1 / $quantidadeEdicoes;
@@ -31,12 +30,12 @@ print_r($anos);
             foreach ($anos as $ano):
 
                 ?>
-                <div class="col col-sm-6 col-md-3">
-                    <a href="anais.php/<?= $ano ?>" style="opacity:<?= $opacidade ?>"
-                        class="mx-4 fs-2 btn rounded-4 botao-anais mx-10 w-45 h-45 text-center py-3 p-5">Anais
-                        <?= $ano ?> </a>
-                </div>
-                <?php
+            <div class="col-12 col-sm-6 col-md-3 py-2">
+                <a href="anais.php/<?= $ano ?>" style="opacity:<?= $opacidade ?>"
+                    class="mx-4 fs-2 btn rounded-4 botao-anais mx-10 w-45 h-45 text-center py-3 p-5">Anais
+                    <?= $ano ?> </a>
+            </div>
+            <?php
                 $opacidade += $porcentagem;
             endforeach;
             ?>
